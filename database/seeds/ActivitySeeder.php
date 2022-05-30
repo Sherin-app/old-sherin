6e5c87873138482b420151ef022fa4c0bd0997f8
topo <hamzaaitsidisaid.11@gmail.com>
2022-05-30 20:45:45 +0200

    seeder activity initial

--- database/seeds/ActivitySeeder.php
+++ database/seeds/ActivitySeeder.php
@@ -13,15 +13,15 @@ class ActivitySeeder extends Seeder
     public function run()
     {
         $activities = [
-            // ['name'=>'Magasin prêt-à-porter', 'description'=>'Magasin prêt-à-porter homme et femme'],
-            // ['name'=>'Parapharmacie', 'description'=>'Ensemble des produits vendus en pharmacie qui ne sont pas des médicaments'],
-            // ['name'=>'Cigarette électronique', 'description'=>'Cigarettes électroniques et d\'e-liquides'],
-            // ['name'=>'Onglerie', 'description'=>'L\'Onglerie vous propose de nombreuses prestations telles que la beauté des mains.'],
-            // ['name'=>'Coiffure', 'description'=>'Coiffure'],
-            // ['name'=>'Scrubs', 'description'=>'Pour protéger les infirmières, vétérinaires et autres personnel soignant.'],
-            // ['name'=>'Animalerie', 'description'=>'Vente des produits pour animaux'],
-            // ['name'=>'Parfumerie', 'description'=>'parfums'],
-            // ['name'=>'Restauration & snack', 'description'=>'Restauration & snack'],
+            ['name'=>'Magasin prêt-à-porter', 'description'=>'Magasin prêt-à-porter homme et femme'],
+            ['name'=>'Parapharmacie', 'description'=>'Ensemble des produits vendus en pharmacie qui ne sont pas des médicaments'],
+            ['name'=>'Cigarette électronique', 'description'=>'Cigarettes électroniques et d\'e-liquides'],
+            ['name'=>'Onglerie', 'description'=>'L\'Onglerie vous propose de nombreuses prestations telles que la beauté des mains.'],
+            ['name'=>'Coiffure', 'description'=>'Coiffure'],
+            ['name'=>'Scrubs', 'description'=>'Pour protéger les infirmières, vétérinaires et autres personnel soignant.'],
+            ['name'=>'Animalerie', 'description'=>'Vente des produits pour animaux'],
+            ['name'=>'Parfumerie', 'description'=>'parfums'],
+            ['name'=>'1', 'description'=>'Restauration & snack'],
             ['id'=>9,'name'=>'Restauration & snack', 'description'=>'Restauration & snack'],
 
         ];
