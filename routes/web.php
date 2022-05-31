913b8ef492369b43acf35c81128efae8100900a2
topo <hamzaaitsidisaid.11@gmail.com>
2022-05-31 16:32:52 +0200

    domaine name web route

--- routes/web.php
+++ routes/web.php
@@ -72,7 +72,7 @@ Route::get('/', function () {
 
 
 
-Route::group(['domain' => 'app.sherin.loc'], function () {
+Route::group(['domain' => 'sherin-old.herokuapp.com'], function () {
     
     //Language Change
 Route::get('lang/{locale}', function ($lang) {
